a71b4ce7d7a0e9c224971e93d35cf92d6d158e7f
Jean-Luc STOUPY <jean-luc.stoupy@etu.unige.ch>
2020-04-02 16:26:12 +0200

    JureCaPasse

--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyComponentTestComponent } from './my-component-test/my-component-test.component';
+import { NotMyComponentComponent } from './not-my-component/not-my-component.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      MyComponentTestComponent
+      MyComponentTestComponent,
+      NotMyComponentComponent
    ],
    imports: [
       BrowserModule,
---
+++ web-ui/src/app/not-my-component/not-my-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-my-component',
+  templateUrl: './not-my-component.component.html',
+  styleUrls: ['./not-my-component.component.css']
+})
+export class NotMyComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
