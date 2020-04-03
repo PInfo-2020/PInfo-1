35ab53f1adf5397268ed8ae7849e6ebc1d6035c6
Rose <rose.defossez@etu.unige.ch>
2020-04-03 14:22:18 +0200

    my bad

---
+++ frigo_service/src/main/java/ch/unige/pickncook/App.java
@@ -0,0 +1,13 @@
+package ch.unige.pickncook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
