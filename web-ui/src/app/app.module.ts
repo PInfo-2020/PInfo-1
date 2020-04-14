75f6fcc540c9ffa898c35a1fc7ee5c52b2aa8100
Jean-Luc STOUPY <jean-luc.stoupy@etu.unige.ch>
2020-04-14 16:28:48 +0200

    Debut du Design de malade

--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -3,18 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyComponentTestComponent } from './my-component-test/my-component-test.component';
-import { NotMyComponentComponent } from './not-my-component/not-my-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuNavComponent } from './menu-nav/menu-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
    declarations: [
       AppComponent,
-      MyComponentTestComponent,
-      NotMyComponentComponent
+      MenuNavComponent
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      LayoutModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatSidenavModule,
+      MatIconModule,
+      MatListModule
    ],
    providers: [],
    bootstrap: [
