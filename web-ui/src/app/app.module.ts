05c5eb2a23b5ca0256d78a8c79ad818a1805b9dd
Jean-Luc STOUPY <jean-luc.stoupy@etu.unige.ch>
2020-04-14 17:10:46 +0200

    FinDeJournée

--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -11,11 +11,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { TrendingReceipesComponent } from './trending-receipes/trending-receipes.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      MenuNavComponent
+      MenuNavComponent,
+      TrendingReceipesComponent
    ],
    imports: [
       BrowserModule,
