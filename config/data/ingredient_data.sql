d41553df028bbcc2e2bc7d19ce1a6b7532bfcabd
Rose <rose.defossez@etu.unige.ch>
2020-04-21 16:43:30 +0200

    correct travis

--- config/data/ingredient_data.sql
+++ config/data/ingredient_data.sql
@@ -1,6 +1,8 @@
+CREATE USER ing WITH PASSWORD 'ing';
 drop sequence if exists INGREDIENT_SEQ;
 create sequence INGREDIENT_SEQ start with 1 increment by 50;
 CREATE TABLE IF NOT EXISTS Ingredient ( id bigint not null, primary key (id), nom varchar(255), poid_moyen bigint, unite varchar(255), categorie varchar(255) );
+GRANT SELECT, INSERT ON ALL TABLES IN SCHEMA public to ing;
 INSERT INTO "Ingredient" VALUES
     (379,'Abricot',60,'unité/g','Fruits/Fruits frais'),
     (469,'Abricot, sec',NULL,'g','Fruits/Fruits secs'),
