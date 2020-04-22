cab2d83016d9288e817cd250c8d3b3d6f22c3c7e
Jimmysburgger <jeanmarc.boutay@gmail.com>
2020-04-22 12:13:52 +0200

    recipe bdd test

--- recipe_service/src/test/resources/META-INF/recipe_test_data.sql
+++ recipe_service/src/test/resources/META-INF/recipe_test_data.sql
@@ -1,1 +1,22 @@
-CREATE TABLE IF NOT EXISTS Recipe ( id bigint not null, primary key (id));
+drop sequence if exists RECIPE_SEQ;
+create sequence RECIPE_SEQ start with 1 increment by 50;
+CREATE TABLE IF NOT EXISTS Recipe (
+	id bigint not null,
+	primary key (id),
+	nom varchar(255),
+	ustensiles varchar(max),
+	tags varchar(max),
+	tempsPreparation smallint,
+	difficulte varchar(255),
+	nbPersonnes smallint,
+	photo varchar(255),
+	preparation varchar(255),
+	auteur bigint,
+	datePublication date,
+	categoriePlat varchar(255),
+	typeCuisine varchar(255),
+	note float(),
+	commentaires bigint
+	
+	
+);
