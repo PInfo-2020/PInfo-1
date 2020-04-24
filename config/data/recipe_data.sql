c71e431c350857f5acbeafb9bceef1efb7d37298
Rose <rose.defossez@etu.unige.ch>
2020-04-24 11:04:05 +0200

    test new bdd

--- config/data/recipe_data.sql
+++ config/data/recipe_data.sql
@@ -5,6 +5,7 @@ drop table IngredientsRecipe if exists;
 drop table Recipe if exists;
 CREATE TABLE IF NOT EXISTS IngredientsRecipe (id serial primary key, id_recipe bigint not null, ingredient bigint not null, quantite smallint, FOREIGN KEY (id_recipe) REFERENCES Recipe (id), UNIQUE (id_recipe, ingredient));
 CREATE TABLE IF NOT EXISTS Recipe (id bigint not null, primary key (id), nom varchar(255), tempsPreparation smallint, difficulte varchar(255),nbPersonnes smallint, photo varchar(255), preparation varchar(255), auteur bigint, datePublication date, categoriePlat varchar(255), typeCuisine varchar(255), note float, commentaires bigint);
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA public to recipe;
 
 TRUNCATE TABLE Recipe; 
 INSERT INTO Recipe (id, nom, tempsPreparation, difficulte, nbPersonnes, photo, preparation, auteur, datePublication, categoriePlat, typeCuisine, note, commentaires) VALUES (RECIPE_SEQ.nextval, 'Abricot sucré', 5, 'facile', 2, 'maPhoto', 'Prenez des abricots, coupez-le en deux, enlevez le noyau, ajoutez du sucre,', 372, '2020-04-22', 'dessert', 'français', 4, 32);
