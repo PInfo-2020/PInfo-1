755aedc9d180b87b85165e75536d37cdffa23109
Jérémie Guy <guyjeremix@gmail.com>
2020-05-07 16:11:15 +0200

    keycloak with bug

--- web-ui/src/main.ts
+++ web-ui/src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { KeycloakService } from './app/services/keycloak/keycloak.service';
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +11,8 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+KeycloakService.init()
+  .then(() => platformBrowserDynamic().bootstrapModule(AppModule))
+  .catch(e => window.location.reload())
